62134e6d28081d1c91d3c90a1610dfeeecd892f4
Rahulyadav051225 <yadavrahulkumar21675@gmail.com>
2026-09-10 16:26:35 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,20 @@
+include<stdio.h>
+int main()
+{
+  int machine = 1' n;
+    printf("enter a number:'\n");
+    scanf("%d", &n);
+    if(n < o)
+{
+   printf(" this is a negative number \n"):
+     }
+else
+{
+for ( int i=1; i<=n; i++)
+{
+machine = machine * i;
+printf("the factorial of this number is %d \n",machine);
+}
+return 0;
+}
+  
